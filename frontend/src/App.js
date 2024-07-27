8589072e38a4bbc791c799d24f9b84b2712dd9a7
atharvaballa <atharvab.balla@gmail.com>
2024-07-27 15:16:48 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function MastermindGame() {
+  // ... your game logic and UI components
+}
+
+export default MastermindGame;
