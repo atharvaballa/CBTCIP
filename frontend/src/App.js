25c873fb4a569073a4583723541152763b3d1fc6
atharvaballa <atharvab.balla@gmail.com>
2024-07-27 15:31:30 +0530

    Delete frontend/src directory

--- frontend/src/App.js
+++
@@ -1,8 +0,0 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-
-function MastermindGame() {
-  // ... your game logic and UI components
-}
-
-export default MastermindGame;
